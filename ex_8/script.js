f729350134fafda0d12fff57cd5ae16554336c48
Vitalik1995Rikov <68601312+Vitalik1995Rikov@users.noreply.github.com>
2021-04-29 22:37:08 +0300

    2.14.1

---
+++ ex_8/script.js
@@ -0,0 +1,7 @@
+if (browser === 'Edge') {
+  alert ('You have got the Edge')
+} else if (browser === 'Chrome' || 'Firefox' || 'Safari') {
+  alert ('We support this browser too')
+} else {
+  alert ('We hope that this page looks ok')
+}
